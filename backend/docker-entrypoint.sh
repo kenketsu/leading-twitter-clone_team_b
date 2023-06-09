d302bfe7f6f3c66d946b047170bc40ff522098a1
kenketsu <razgriz.memai02723@gmail.com>
2023-06-10 02:34:58 +0900

    healthcheckを利用

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -5,19 +5,12 @@ if [ -n "$1" ]; then
     exec "$@"
 fi
 
-echo "===== Connecting to PostgreSQL ====="
-until export PGPASSWORD=$POSTGRES_PASSWORD && psql -U $POSTGRES_USER -h $POSTGRES_HOST; do
-    echo "***** Waiting *****"
-    sleep 1
-done
-echo "============ Connected! ============"
-
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 
 if [ "${DJANGO_SUPERUSER_AUTO_CREATE}" = "True" ]; then
-    python manage.py createsuperuser --noinput
+    python manage.py custom_createsuperuser --noinput
 fi
 
 python manage.py runserver 0.0.0.0:8000
