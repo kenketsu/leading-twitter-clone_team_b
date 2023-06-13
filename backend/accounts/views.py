a2253762f96f2d197e97c5cbc454e24c916ce136
kenketsu <razgriz.memai02723@gmail.com>
2023-06-13 22:52:54 +0900

    空行を追加

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics, permissions
+
 from .serializers import UserCreateSerializer
+
 
 class UserCreateAPIView(generics.CreateAPIView):
     serializer_class = UserCreateSerializer
