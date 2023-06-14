78d24aed4ee3833c9786944af40a550c7552e857
pomu <dailysentence1111@gmail.com>
2023-06-14 19:50:52 +0900

    react-router-dom test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import './App.css';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Header } from './Header';
+import { Top } from './Top';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          完了
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export const App = () => {
+  return(
+    <div>
+      <Router>
+        <Header />
+        <div>
+          <Switch>
+              <Route exact path='/' component={Top} />
+              <Route exact path='/accounts' component={Accounts} />
+              <Route render={() => <h4>not found...</h4>} />
+          </Switch>
+        </div>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
