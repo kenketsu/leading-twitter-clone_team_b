c09993d53260a7b1dae8a7e86f5a80af451ead76
Hanaka Iwasaki <h.iwasaki.22.9.631@4nonome.com>
2023-06-21 00:18:14 +0900

    signup関数

--- frontend/src/twitter/pages/Signup.js
+++ frontend/src/twitter/pages/Signup.js
@@ -1,35 +1,63 @@
-import React from 'react';
+import React,{ useState } from 'react';
+import signUpApi from '../../api/signUpApi'
 
 function Signup() {
-  return (
-    <main className="form-signin">
-      <form>
-        <h1 className="h3 mb-3 fw-normal">Sign Up</h1>
+  const [users, setUsers] = useState([])
+  const [newUser, setNewUser] = useState('')
 
-        <div className="form-floating">
-          <input className="form-control" placeholder="ユーザー名" />
-          {/* <label>First Name</label> */}
-        </div>
+  const addUser = async (e) => {
+    e.preventDefault()
+    try {
+      const user = await signUpApi.createUser(newUser)
+      setUsers([...users, user])
+      setNewUser('')
+    } catch (error) {
+      // console.error(error)
+    }
+  }
 
-        <div className="form-floating">
-          <input type="email" className="form-control" placeholder="email" />
-          {/* <label>Email address</label> */}
-        </div>
+return (
+  <main className="form-signup">
+    <form>
+      <h1 className="h3 mb-3 fw-normal">Sign Up</h1>
 
-        <div className="form-floating">
-          <input type="password" className="form-control" placeholder="password" />
-          {/* <label>Password</label> */}
-        </div>
+      <div className="form-floating">
+        <input type="text"
+        className="form-control"
+        placeholder="ユーザー名" />
+        {/* <label>First Name</label> */}
+      </div>
 
-        <div className="form-floating">
-          <input type="password" className="form-control" placeholder="Re password" />
-          {/* <label>Password Confirm</label> */}
-        </div>
+      <div className="form-floating">
+        <input type="email"
+        className="form-control"
+        placeholder="email" />
+        {/* <label>Email address</label> */}
+      </div>
 
-        <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
-      </form>
-    </main>
-  );
+      <div className="form-floating">
+        <input type="password"
+        className="form-control"
+        placeholder="password" />
+        {/* <label>Password</label> */}
+      </div>
+
+      <div className="form-floating">
+        <input type="password"
+        className="form-control"
+        placeholder="Re password" />
+        {/* <label>Password Confirm</label> */}
+      </div>
+
+      <button className="w-100 btn btn-lg btn-primary"
+      type="submit"
+      onClick={addUser}
+      >
+        Submit
+      </button>
+    </form>
+  </main>
+)
 }
 
 export default Signup;
