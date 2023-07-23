f39d4afd89a8c22b34a5dc6ecd5e798c0de2991e
Hanaka Iwasaki <h.iwasaki.22.9.631@4nonome.com>
2023-07-23 11:38:31 +0900

    07/23進捗 レビュー修正

--- frontend/src/twitter/pages/Signup.js
+++ frontend/src/twitter/pages/Signup.js
@@ -9,8 +9,6 @@ import Button from "@mui/material/Button";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
